4a1d5399ec82cd69cd4b1598c88e394ea4660e99
dmitryemacs <dimadimof81@gmail.com>
2026-01-29 23:38:15 -0800

    update configuration

--- init.el
+++ init.el
@@ -1,286 +1,201 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;OPTIONAL SETTING;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-						 ("org" . "https://orgmode.org/elpa/")
-						 ("elpa" . "https://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
-;;BASIC UI CONFIGURATION
-(setq inhibit-startup-message t)
-(setq initial-buffer-choice nil)
+;; Set name
+(setq frame-title-format "Emacs")
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq org-directory "~/Notes")
+(setq org-agenda-files (list org-directory))
 
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(menu-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)
-(column-number-mode)
-(global-display-line-numbers-mode t)
 
-;;Blinking cursor
-(setq blink-cursor-blinks 0)
+;; Environment settings
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+(tab-bar-mode 0)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-;; tabs
-(setq-default tab-width          4)
-(setq-default c-basic-offset     4)
-(setq-default standart-indent    4)
+;; Number mode
+(column-number-mode 1)
+(show-paren-mode 1)
+(global-display-line-numbers-mode 1)
 
-;;image in org mode size
-(setq org-image-actual-width 600)
-
-;; Set up the visible bell
-(setq visible-bell t)
-
-;;colors CURSOR
-(set-frame-parameter nil 'cursor-color "#ffffff")
-(add-to-list 'default-frame-alist '(cursor-color . "#ffffff"))
-
-;;line-numbers-mode off 
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                shell-mode-hook
-                treemacs-mode-hook
-				pdf-view-mode-hook
-                eshell-mode-hook
-				nov-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;;font
-;;(set-face-attribute 'default nil :font "Iosevka Nerd Font 12")
-(set-face-attribute 'default nil :font "Hack Nerd Font-11")
-
-;;daemon fonts
-(defun efs/set-font-faces ()
-  (message "Setting faces!")
-  (set-face-attribute 'default nil :font "Hack Nerd Font-11")
-  ;; Set the fixed pitch face
-  (set-face-attribute 'fixed-pitch nil :font "Hack Nerd Font-11")
-  ;; Set the variable pitch face
-  (set-face-attribute 'variable-pitch nil :font "Hack Nerd Font-11"))
-
-(if (daemonp)
-    (add-hook 'after-make-frame-functions
-              (lambda (frame)
-				(dashboard-open)
-                (with-selected-frame frame
-                  (efs/set-font-faces))))
-  (efs/set-font-faces))
-
-;;set "gnu" style for c
-(setq c-deafault-style "linux"
-      c-basic-offset 4)
-
-;;garbage
-(setq gc-cons-threshold (* 10 1000 1000))
-(setq gc-cons-percentage 0.6)
-
-;;auto pair
+;; Pair mode
 (electric-pair-mode 1)
 
-;; view image in org mode
-(setq org-src-fontify-natively 't)
-(setq org-startup-with-inline-images t)
-
-;; Scroll
+;; Scroll settings
 (setq scroll-step 1)
 (setq scroll-conservatively 10000)
 (setq auto-window-vscroll nil)
-;; Scroll Mouse
-(pixel-scroll-precision-mode t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;MAPPING;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Cursor settings
+(setq blink-cursor-blinks 0)
+(blink-cursor-mode 1)
 
-;; Настраиваем перемещение для русской раскладки
-(defun setup-ru-layout-keys ()
-  "Переназначить клавиши перемещения для русской раскладки."
-  (let ((key-remap '(
-                     ;; Перемещение
-                     ("C-а" . "C-f")  ;; Вперёд (forward-char)
-                     ("C-и" . "C-b")  ;; Назад (backward-char)
-                     ("C-т" . "C-n")  ;; Вниз (next-line)
-                     ("C-з" . "C-p")  ;; Вверх (previous-line)
-					 ("C-ф" . "C-a")  ;; В начало строки
-					 ("C-у" . "C-e")  ;; В конец строки
-                     ;; Удаление
-                     ("C-в" . "C-d")  ;; Удалить символ (delete-char)
-                     ("C-л" . "C-k")  ;; Удалить строку (kill-line)
+;; Font
+(set-face-attribute 'default nil :font "Hack Nerd Font-12")
 
-					 ;; Перемещение по словам
-                     ("M-а" . "M-f")  ;; перемещение по слову вмеред (kill-line)
-					 ("M-и" . "M-b")
+;; Org agenda directory
+(setq org-agenda-files '("D:/Documents/Notes"))
 
-                     )))
-    (dolist (pair key-remap)
-      (define-key key-translation-map (kbd (car pair)) (kbd (cdr pair))))))
+;; Custom cursor face
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cursor ((t (:background "white")))))
 
-(setup-ru-layout-keys)
-
-(global-set-key (kbd "C-M-p") 'windmove-up)
-(global-set-key (kbd "C-M-n") 'windmove-down)
-(global-set-key (kbd "C-M-b") 'windmove-left)
-(global-set-key (kbd "C-M-f") 'windmove-right)
-
-;;resize window
-(defun enlarge-vert ()
-  (interactive)
-  (enlarge-window 4))
-
-(defun shrink-vert ()
-  (interactive)
-  (enlarge-window -4))
-
-(defun enlarge-horz ()					
-  (interactive)
-  (enlarge-window-horizontally 4))
-
-(defun shrink-horz ()
-  (interactive)
-  (enlarge-window-horizontally -4))
-
-(define-prefix-command 'my-mapping)
-(define-key my-mapping (kbd "C-c k") 'shrink-vert)
-(define-key my-mapping (kbd "C-c i") 'enlarge-vert)
-(define-key my-mapping (kbd "C-c j") 'shrink-horz)
-(define-key my-mapping (kbd "C-c l") 'enlarge-horz)
-
-(define-prefix-command 'window-resize-map)
-(global-set-key (kbd "C-x w") 'window-resize-map)
-
-(define-key window-resize-map (kbd "p") (lambda () (interactive) (enlarge-window 4)))
-(define-key window-resize-map (kbd "n") (lambda () (interactive) (enlarge-window -4)))
-(define-key window-resize-map (kbd "f") (lambda () (interactive) (enlarge-window-horizontally 4)))
-(define-key window-resize-map (kbd "b") (lambda () (interactive) (enlarge-window-horizontally -4)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;PACKAGES;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Theme gruber-darker
-(use-package gruber-darker-theme
-  :ensure t
-  :config
-  (load-theme 'gruber-darker t))
-
-(use-package doom-modeline
-  :ensure t
-  :config
-  (doom-modeline-mode))
-
-;; Company
-(use-package company
-  :ensure t
-  :config
-  (add-hook 'after-init-hook 'global-company-mode))
-
-;; Ivy and Counsel
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(use-package ivy-rich
-  :after ivy
-  :init
-  (ivy-rich-mode 1))
-
-(use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (counsel-mode 1))
-
-;; Lsp servers
-(use-package lsp-mode
-  :ensure t
-  :hook
-  (c++-mode . lsp) 
-  (java-mode . lsp)
-  (c-mode . lsp)
-  (js-mode . lsp))
-
-(use-package lsp-ui
-  :ensure t
-  :after lsp-mode
-  :custom
-  (lsp-ui-doc-show-with-cursor t))
-
-;; Improved candidate sorting with prescient.el 
-(use-package ivy-prescient
-  :after counsel
-  :custom
-  (ivy-prescient-enable-filtering nil)
-  :config
-  (ivy-prescient-mode 1))
-
-;; Git
-(use-package magit
-  :commands magit-status
-  :custom
-  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-(use-package forge
-  :after magit)
-
-;; Startup logo
 (use-package dashboard
   :ensure t
   :config
-  (dashboard-setup-startup-hook)
-  (setq dashboard-center-content t)
-  (setq dashboard-items '((recents  . 10)))
-  ;;(projects . 5)
-  ;;(agenda   . 5)))
-  (setq dashboard-banner-logo-title "Welcome to Emacs!"))
+  ;; Set the title
+  (setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
+  ;; Set the banner
+  ;;(setq dashboard-startup-banner [VALUE])
+  ;; Value can be:
+  ;;  - 'official which displays the official emacs logo.
+  ;;  - 'logo which displays an alternative emacs logo.
+  ;;  - an integer which displays one of the text banners
+  ;;    (see dashboard-banners-directory files).
+  ;;  - a string that specifies a path for a custom banner
+  ;;    currently supported types are gif/image/text/xbm.
+  ;;  - a cons of 2 strings which specifies the path of an image to use
+  ;;    and other path of a text file to use if image isn't supported.
+  ;;    (cons "path/to/image/file/image.png" "path/to/text/file/text.txt").
+  ;;  - a list that can display an random banner,
+  ;;    supported values are: string (filepath), 'official, 'logo and integers.
 
-;; pdf-tools
-(use-package pdf-tools
+  ;; Content is not centered by default. To center, set
+  (setq dashboard-center-content t)
+  ;; vertically center content
+  (setq dashboard-vertically-center-content t)
+  (setq dashboard-show-shortcuts nil)
+  (dashboard-setup-startup-hook))
+
+(use-package gptel
   :ensure t
   :config
-  (add-hook 'find-file-hook
-			(lambda ()
-              (when (string-match-p "\\.pdf$" buffer-file-name)
-				(pdf-tools-install)))))
+  (setq gptel-model   'mistralai/devstral-2512:free
+	gptel-backend
+	(gptel-make-openai "Astral"
+          :host "openrouter.ai"
+          :endpoint "/api/v1/chat/completions"
+          :stream t
+          :key "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"
+          :models '(mistralai/devstral-2512:free)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;; Mapings
+  (global-set-key (kbd "C-c C-r") 'gptel-rewrite)
+  (global-set-key (kbd "C-<return>") 'gptel-send))
+
+;; Packages with use-package
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode 1))
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+
+(use-package yasnippet-snippets
+  :ensure t)
+
+(use-package ivy
+  :ensure t
+  :init
+  (ivy-mode 1))
+
+(use-package swiper
+  :ensure t
+  :bind (("C-s" . swiper)))
+
+(use-package magit
+  :ensure t)
+
+(use-package ace-window
+  :ensure t
+  :init
+  (setq aw-ignore-minibuffer t)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (setq aw-background "darkred")
+  (setq aw-scale 10.0)
+  :bind (("C-o" . ace-window)))
+
+(use-package gruber-darker-theme
+  :ensure t
+  :init
+  (load-theme 'gruber-darker t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; USER FUNCTIONS AND MAPINGS
+
+;; c-mode settings
+(setq-default c-basic-offset 4
+              c-default-style '((java-mode . "java")
+                                (awk-mode . "awk")
+                                (other . "bsd")))
+(add-hook 'c-mode-hook (lambda ()
+                         (interactive)
+                         (c-toggle-comment-style -1)))
+
+;; Go-mode settings
+;; Основные настройки для go-mode
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq tab-width 4)
+            (setq go-indent-offset 4)
+            (add-hook 'before-save-hook 'gofmt-before-save nil t)))
+
+;; compilation
+(defun my/compilation-setup ()
+  (setq compilation-window-height 20)
+  (setq display-buffer-alist
+        '(("\\*compilation\\*"
+           (display-buffer-below-selected)
+           (window-height . fit-window-to-buffer)))))
+
+(add-hook 'compilation-mode-hook 'my/compilation-setup)
+
+;; map
+(global-set-key (kbd "C-c c") 'compile)
+
+;; Org-agenda mapings
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c t") 'org-todo-list)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; Ensure packages are installed
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;;'(initial-buffer-choice "~/")
- '(initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+ '(custom-enabled-themes '(doom-monokai-spectrum))
+ '(custom-safe-themes
+   '("be0d9f0e72a4ebc4a59c382168921b082b4dc15844bdaf1353c08157806b3321"
+     "19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd"
+     "f6ea954a9544b0174a876d195387f444da441535ee88c7fb0fc346af08b0d228"
+     "c9d837f562685309358d8dc7fccb371ed507c0ae19cf3c9ae67875db0c038632"
+     "6963de2ec3f8313bb95505f96bf0cf2025e7b07cefdb93e3d2e348720d401425"
+     "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     "9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
+     default))
  '(package-selected-packages
-   '(pdf-tools annalist company counsel dashboard doom-modeline forge goto-chg gruber-darker-theme helm-core ivy-prescient ivy-rich llm lsp-java lsp-ui org-bullets org-pdftools org-present queue shell-maker wfnames))
- '(warning-suppress-log-types '((evil-collection))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
+   '(ace-window company dashboard docker-compose-mode dockerfile-mode
+		doom-themes go-mode gptel gruber-darker-theme lsp-mode
+		magit neotree projectile swiper yasnippet
+		yasnippet-snippets)))
